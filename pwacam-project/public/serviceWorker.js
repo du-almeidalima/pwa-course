5332cf67c8710f53fc046f94a1a9797bd9c636e2
Eduardo de Almeida Lima <eduardo.lima@venturus.org.br>
2022-05-04 10:55:03 -0300

    Section 3: Listen to Non-Lifecycle Events (Fetch)

--- pwacam-project/public/serviceWorker.js
+++ pwacam-project/public/serviceWorker.js
@@ -1,13 +1,24 @@
+const LOGGER_NAME = '[Service Worker]:'
+
 // Instead of "this" to refer to the Service Worker
 // Remembering that the Service Worker runs on a separate thread and don't have access to the DOM
 self.addEventListener('install', (e) => {
-    console.log('[Service Worker]: Installing Service Worker ...', e)
+    console.log(`${LOGGER_NAME} Installing Service Worker ...`, e)
 })
 
 self.addEventListener('activate', (e) => {
-    console.log('[Service Worker]: Activating Service Worker ...', e)
+    console.log(`${LOGGER_NAME} Activating Service Worker ...`, e)
 
     // Pages loaded before this service worker was registered will not be controlled by it. This claims the control over
     // those pages.
     return self.clients.claim();
+})
+
+// Event triggered by assets loading (.js, .css, .html, .png, etc...) and using the Fetch API.
+// INFO: Calls using XMLHttpRequest won't trigger this listener
+self.addEventListener('fetch', (e) => {
+    console.log(`${LOGGER_NAME} Fetching something...`, e)
+
+    // Overriding what the request response will be!
+    e.respondWith(fetch(e.request));
 })
